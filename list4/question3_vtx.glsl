07b9cdcd5440dd0b62a9fc5bab848181c93910e4
Adriano Paulichi <adriano.paulichi@localiza.com>
2023-04-06 16:59:36 -0300

    Added list 4 exercises

---
+++ list4/question3_vtx.glsl
@@ -0,0 +1,13 @@
+#version 330 core
+layout (location = 0) in vec3 position;
+layout (location = 1) in vec3 color;
+
+out vec3 vColor;
+
+uniform mat4 MVP;
+
+void main()
+{
+    gl_Position = MVP * vec4(position, 1.0);
+    vColor = color;
+}
